35a473fc07de4f45e3344f573801830b950fe55a
Mauricio <mptorrrejon@gmail.com>
2013-12-17 20:46:07 -0500

    Update RigidBodySimulator.cpp

--- CppRigidBodyPFP/src/RigidBodySimulator.cpp
+++ CppRigidBodyPFP/src/RigidBodySimulator.cpp
@@ -1,48 +1,52 @@
 #include "RigidBodySimulator.hpp"
 
-RigidBodySimulator::RigidBodySimulator(void) //original/default constructor 
-{
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+
+int size (char* line);
+RigidBodySimulator::RigidBodySimulator(void){
 	//must use pushback-> insert single sub-robot configuration into super-robot at default configuration
-	m_robot.m_x.push_back(0);
-	m_robot.m_y.push_back(0);
-	m_robot.m_theta.push_back(0);
 	
-	m_circles.push_back(16); //goal x
-	m_circles.push_back(-7); //goal y
-	m_circles.push_back(1.0); //goal radius
+	/*for now...*/
+    //m_robot.m_x.push_back(0);
+    //m_robot.m_y.push_back(0); 
+    //m_robot.m_theta.push_back(0);    
+    
+    m_circles.push_back(16);
+    m_circles.push_back(-7);
+    m_circles.push_back(1.0);  
 }
 
-RigidBodySimulator::RigidBodySimulator(int number_of_bots) //
-{
-	//change to initialize every robot at a different location, positioned across the workspace at a uniform-random distribution
-	/*NOTE: number of robots not known til ReadRobot is called,
-	must create a way to find it by time of or on constructor call*/
-	for(int i=0; i<number_of_bots; i++)
-	{
-		//workspace goes from (-22,-14) on bottom left to (22,14) on top right
-		m_robot.m_x.push_back(pow(-1,rand()%2)*rand()%22); /*rand using x-range coordinates -22~22*/
-		m_robot.m_y.push_back(pow(-1,rand()%2)*rand()%14); /*rand using y-range coordinates* -14~14*/
-		/*should replace rand() above with function that returns a float/double instead of int for better implementation*/
-		m_robot.m_theta.push_back(0);
-		
-		/**WILL ALSO NEED TO CHECK EACH NEW SUB-ROBOT FOR COLLISION BEFORE ADDING IT TO SUPER-ROBOT
-		PROBABLY GENERATE PARAMETERS FIRST, CHECK FOR COLLISION, THEN
-		IF NO COLLISION -> PUSH_BACK PARAMETERS, ELSE DO NOTHING*/
-	}
-	m_circles.push_back(16); //goal x
-	m_circles.push_back(-7); //goal y
-	m_circles.push_back(1.0); //goal radius
+RigidBodySimulator::RigidBodySimulator(int number_of_bots){
+        //change to initialize every robot at a different location, positioned across the workspace at a uniform-random distribution
+        /*NOTE: number of robots not known til ReadRobot is called,
+        must create a way to find it by time of or on constructor call*/
+        for(int i=0; i<number_of_bots; i++){
+                //workspace goes from (-22,-14) on bottom left to (22,14) on top right
+                m_robot.m_x.push_back((int)pow(-1,rand()%2)*rand()%22); /*rand using x-range coordinates -22~22*/
+                m_robot.m_y.push_back((int)pow(-1,rand()%2)*rand()%14); /*rand using y-range coordinates* -14~14*/
+                /*should replace rand() above with function that returns a float/double instead of int for better implementation*/
+                m_robot.m_theta.push_back(0);
+                
+                /**WILL ALSO NEED TO CHECK EACH NEW SUB-ROBOT FOR COLLISION BEFORE ADDING IT TO SUPER-ROBOT
+                PROBABLY GENERATE PARAMETERS FIRST, CHECK FOR COLLISION, THEN
+                IF NO COLLISION -> PUSH_BACK PARAMETERS, ELSE DO NOTHING*/
+        }
+        m_circles.push_back(16); //goal x
+        m_circles.push_back(-7); //goal y
+        m_circles.push_back(1.0); //goal radius
 }
 
-RigidBodySimulator::~RigidBodySimulator(void)
-{
-}
+RigidBodySimulator::~RigidBodySimulator(void){}
 
-//no change for this method as the x,y coordinates are passed in
-	//I think...
-	//need to investigate where it's being called
-Point RigidBodySimulator::ClosestPointOnObstacle(const int i, const double x, const double y)
-{
+Point RigidBodySimulator::ClosestPointOnObstacle(const int i, const double x, const double y){
     const double cx = m_circles[3 + 3 * i];
     const double cy = m_circles[4 + 3 * i];
     const double r  = m_circles[5 + 3 * i];
@@ -56,38 +60,33 @@ Point RigidBodySimulator::ClosestPointOnObstacle(const int i, const double x, co
     return p;
 }
 
-void RigidBodySimulator::AddToRobotConfiguration(int r, const double dx, const double dy, const double dtheta)
-{
-	//used 'sub_robot' instead of 'i' for the robot index here since 'i' is used in the for-loop
-	m_robot.m_x[r] += dx;
-	m_robot.m_y[r] += dy;
-	m_robot.m_theta[r] += dtheta;
+void RigidBodySimulator::AddToRobotConfiguration(int r_index, const double dx, const double dy, const double dtheta){
+    m_robot.m_x[r_index] += dx;
+    m_robot.m_y[r_index] += dy;
+    m_robot.m_theta[r_index] += dtheta;
     
-    const double ctheta = cos(m_robot.m_theta[r]);
-    const double stheta = sin(m_robot.m_theta[r]);
-    const int    n      = m_robot.m_currVertices[r].size();
+    const double ctheta = cos(m_robot.m_theta[r_index]);
+    const double stheta = sin(m_robot.m_theta[r_index]);
+    const int    n      = m_robot.m_currVertices[r_index].size();
+    //const int robots = m_robot.m_
     
-    for(int i = 0; i < n; i += 2)
-    {
-	m_robot.m_currVertices[r][i] = 
-	    ctheta * m_robot.m_initVertices[r][i] -
-	    stheta * m_robot.m_initVertices[r][i + 1] + m_robot.m_x[r];
-	
-	m_robot.m_currVertices[r][i + 1] = 
-	    stheta * m_robot.m_initVertices[r][i] +
-	    ctheta * m_robot.m_initVertices[r][i + 1] + m_robot.m_y[r];
+    for(int i = 0; i < n; i += 2){
+		
+		m_robot.m_currVertices[r_index][i] = 
+			ctheta * m_robot.m_initVertices[r_index][i] -
+			stheta * m_robot.m_initVertices[r_index][i + 1] + m_robot.m_x[r_index];
+		
+		m_robot.m_currVertices[r_index][i + 1] = 
+			stheta * m_robot.m_initVertices[r_index][i] +
+			ctheta * m_robot.m_initVertices[r_index][i + 1] + m_robot.m_y[r_index];
     }
-
 }
 
-//new function
+
 void RigidBodySimulator::ReadRobot(const char fname[]){
     FILE *in = fopen(fname, "r");
     
-    int   n  = 0;
-    int num;
-    int i=0;
-    int line[n]; //will store all vertices of the total #-of robots
+    int n  = 0, num = 0, i=0;
     
     if(in){
 		// n -should print the number of vertices read 
@@ -95,84 +94,56 @@ void RigidBodySimulator::ReadRobot(const char fname[]){
 		if(fscanf(in, "%d", &n ) != 1)
 			return;
 		// n, stores the number of robots 
-		printf("\nNumber of Robots: %d===========================>", n);
+		printf("\nNumber of Robots: ===========================> <%d>", n);
+		int line[n]; //will store all vertices of the total #-of robots
 		
+		for(int v=0; v<n; v++)	line[v] = 0;
 		//gets the line of vertices
-		while( i != n && fscanf(in, "%d", &num) > 0  ){
-			line[i] = num;	
-			printf("\nRobot <%d> has <%d> Vertices", i, line[i]);
-			i++;
+		for (int m=0; m<n; m++){
+			fscanf(in, "%d", &num) ; 
+			line[m] = num;	
+			printf("\nRobot <%d> has <%d> Vertices", m, line[m]);;
 		}
 		
-		//line[i] = n
-    
-		for (int j=0; j<n; j++){
-			m_robot.m_currVertices.resize(2 * line[j]);
+		m_robot.m_currVertices.resize(n);
+		m_robot.m_triangles.resize(n );
+		m_robot.m_initVertices.resize(n);
+		
+		for (int j=0; j<n; j++){			
+			m_robot.m_currVertices[j].resize(2 * line[j]);
 			
-			printf("\nNumber of vertices <%d> ", line[j]);       
+			printf("\nNumber of vertices <%d> for Robot: %d", line[j], j);       
 			
 			for(i = 0; i < 2 * line[j]; i++){
-				if(fscanf(in, "%lf", &(m_robot.m_currVertices[i])) != 1)	
+				if(fscanf(in, "%lf", &(m_robot.m_currVertices[j][i])) != 1)	
 					return;
 					
-				printf("\n %f", m_robot.m_currVertices[i] );
+				//printf("\n%f -->", m_robot.m_currVertices[j][11] );
+				printf("\nRobot: %d - i:%d %f",j,i, m_robot.m_currVertices[j][i] );
 			}
 			
-			m_robot.m_triangles.resize(3 * (line[j] - 2) );
-			printf("\nNumber of Triangles: %d\n",(int) m_robot.m_triangles.size());
+			m_robot.m_triangles[j].resize(3 * (line[j] - 2) );
 			
-			for(int k = 0; k < (int) m_robot.m_triangles.size(); ++k)
-				if(fscanf(in, "%d", &(m_robot.m_triangles[k])) != 1)
+			printf("\nNumber of Triangles: %d for Robot: %d\n",(int) m_robot.m_triangles[j].size(), j);
+			
+			for(int k = 0; k <   (int)m_robot.m_triangles[j].size(); ++k)
+				if(fscanf(in,"%lf", &(m_robot.m_triangles[j][k]) ) != 1)
 					return;
 				
 				else
-					printf(" %d ", m_robot.m_triangles[k]);
+					printf("%d ", (int)m_robot.m_triangles[j][k]);
 			
-			 m_robot.m_initVertices.assign(m_robot.m_currVertices.begin(),m_robot.m_currVertices.end());
-		
+			printf("\n\n");
+			
+			 m_robot.m_initVertices[j].assign(	
+									m_robot.m_currVertices[j].begin(),
+									m_robot.m_currVertices[j].end());
+			
+			
 		}// end of FOR-LOOP
-    
-        fclose(in);            
+		
+        fclose(in);     
         
     }        
     else 	printf("..could not open file <%s>\n", fname);    
 }//end of READ-ROBOT
-
-
-
-
-/*
-void RigidBodySimulator::ReadRobot(const char fname[])
-{
-    FILE *in = fopen(fname, "r");
-    int   n  = 0;
-    
-    if(in)
-    {
-	if(fscanf(in, "%d", &n) != 1)
-	    return;
-    
-	m_robot.m_currVertices.resize(2 * n);	    
-	for(int i = 0; i < 2 * n; ++i)
-	    if(fscanf(in, "%lf", &(m_robot.m_currVertices[i])) != 1)
-		return;
-    
-	m_robot.m_triangles.resize(3 * (n - 2));
-	for(int i = 0; i < (int) m_robot.m_triangles.size(); ++i)
-	    if(fscanf(in, "%d", &(m_robot.m_triangles[i])) != 1)
-		return;
-	    else
-		printf("%d ", m_robot.m_triangles[i]);
-	printf("\n...done with tris\n");
-	
-    
-  
-	m_robot.m_initVertices.assign(m_robot.m_currVertices.begin(),
-				      m_robot.m_currVertices.end());
-	fclose(in);	    
-    }	
-    else
-	printf("..could not open file <%s>\n", fname);    
-}
-*/
-
